b9f55fed247ddea019c9554ff47070e0fdfde4eb
ramon torres carrero <ramon.torres.81@gmail.com>
2021-05-09 15:21:59 +0200

    socket

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/chat", (req, res) => {
+router.get('/chat', (req, res) => {
   //const params = { user: { email: req.user.email, nick: req.user.nick } };
   const params = { user: { nick: req.user.nick } };
   res.render("chat/chat", params);
