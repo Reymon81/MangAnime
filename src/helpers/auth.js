5885b4e0b9991fab25f1109987186df7e26923f7
ramon torres carrero <ramon.torres.81@gmail.com>
2021-05-23 13:40:18 +0200

    tamaño lista usuarios

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,7 +4,7 @@ helpers.isAuthenticated = (req, res, next) => {
     if(req.isAuthenticated()){
         return next();
     }
-    req.flash('error', 'debe loguearse');
+    req.flash('error', 'debe loguearse para poder acceder a esa pagina');
     res.redirect('/users/signin');
 };
 
